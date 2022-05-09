9f62b82d8e4eafdd2979c71a7d155d29249a6f9c
Matidev <matidev.kumar@capgemini.com>
2022-05-09 23:02:04 +0530

    initial ddl statements

---
+++ database/dml/dml_matidev_yps_cg_training_shank.sql
@@ -0,0 +1,32 @@
+/*  
+    Object name:    dml_matidev_yps_cg_training_shank.sql
+    Object type:    table
+    Purpose:        This table will hold the details of all past and present managers
+                    of the user
+    Created by:     Matidev Kumar
+    Created on:     9th May 2022
+    ____________________________________________________________________
+    Last modified by    |       Modified on     |   Modification history
+    ____________________________________________________________________
+    Matidev Kumar     |       9th May 2022    |   Initial version   
+    ____________________________________________________________________
+*/
+
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Satyam Rout', 'Nawrin Sayed', '19-AUG-2021', '19-AUG-2021', '30-APR-2022','N');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Satyam Rout', 'Shashank Jain', '19-AUG-2021', '01-MAY-2022', NULL,'Y');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Arjunjyoti Saha', 'Nawrin Sayed', '19-AUG-2021', '19-AUG-2021', '30-APR-2022','N');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Arjunjyoti Saha', 'Shashank Jain', '19-AUG-2021', '01-MAY-2022', NULL,'Y');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Abhijit Bengani', 'Nawrin Sayed', '19-AUG-2021', '19-AUG-2021', '30-APR-2022','N');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Abhijit Bengani', 'Shashank Jain', '19-AUG-2021', '01-MAY-2022', NULL,'Y');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Nipun Jain', 'Nawrin Sayed', '19-AUG-2021', '19-AUG-2021', '30-APR-2022','N');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Nipun Jain', 'Shashank Jain', '19-AUG-2021', '01-MAY-2022', NULL,'Y');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Kshitij Pal', 'Nawrin Sayed', '19-AUG-2021', '19-AUG-2021', '30-APR-2022','N');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Kshitij Pal', 'Shashank Jain', '19-AUG-2021', '01-MAY-2022', NULL,'Y');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Abhishek Kumar', 'Nawrin Sayed', '19-AUG-2021', '19-AUG-2021', '30-APR-2022','N');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Abhishek Kumar', 'Shashank Jain', '19-AUG-2021', '01-MAY-2022', NULL,'Y');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Akshat Agarwal', 'Nawrin Sayed', '19-AUG-2021', '19-AUG-2021', '30-APR-2022','N');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Akshat Agarwal', 'Shashank Jain', '19-AUG-2021', '01-MAY-2022', NULL,'Y');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Arpita Kanungoe', 'Nawrin Sayed', '19-AUG-2021', '19-AUG-2021', '30-APR-2022','N');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Arpita Kanungoe', 'Shashank Jain', '19-AUG-2021', '01-MAY-2022', NULL,'Y');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Seema', 'Nawrin Sayed', '19-AUG-2021', '19-AUG-2021', '30-APR-2022','N');
+INSERT INTO matidev_yps_cg_training_shank VALUES ('Seema', 'Shashank Jain', '19-AUG-2021', '01-MAY-2022', NULL,'Y');
